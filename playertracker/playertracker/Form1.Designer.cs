4c4240cce11a7cb8c836e43b09fce912e366b793
Steven Havko <stevenhavko@gmail.com>
2015-10-10 08:51:02 -0400

    Initial Commit

---
+++ playertracker/playertracker/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace playertracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtStats = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(375, 202);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "button1";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtStats
+            // 
+            this.txtStats.Enabled = false;
+            this.txtStats.Location = new System.Drawing.Point(146, 312);
+            this.txtStats.Name = "txtStats";
+            this.txtStats.Size = new System.Drawing.Size(428, 20);
+            this.txtStats.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 543);
+            this.Controls.Add(this.txtStats);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtStats;
+    }
+}
+
---
+++ playertracker/playertracker/stats.py
@@ -0,0 +1,8 @@
+﻿import nflgame
+
+
+
+def playerStats():
+	game = nflgame.one(2015, 1, "NE", "PIT")
+	tom_brady = game.players.name("T.Brady")
+	return tom_brady.formatted_stats()
